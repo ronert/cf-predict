3ed8db3087789bcaf9d48a37017f499b0cbeb131
Ronert Obst <ronert.obst@gmail.com>
2016-03-06 12:11:50 +0100

    Use integration test config

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,10 @@
 """Integration tests configuration file."""
-
+import pytest
 from cf_predict.test.conftest import pytest_configure  # pylint: disable=unused-import
+from cf_predict import create_app
+
+
+@pytest.fixture
+def app():
+    app = create_app("integration_testing")
+    return app
