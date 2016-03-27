2cb1e6ee45475cc635bfc4741a309a228dbbc51c
Ronert Obst <ronert.obst@gmail.com>
2016-03-27 13:31:55 +0200

    Bump version: 0.1.0 → 0.1.1

--- cf_predict/__init__.py
+++ cf_predict/__init__.py
@@ -8,7 +8,7 @@ from .config import config
 from .api import api_bp
 
 __project__ = 'cf-predict'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 VERSION = "{0} v{1}".format(__project__, __version__)
 
